51179ff3912d13495b67791917cd78ab9ca09334
ottogudens <ottogudenschwager@gmail.com>
2026-09-14 22:32:27 -0300

    chore: remove temp py scripts

--- fix_chat_screen2.py
+++
@@ -1,8 +0,0 @@
-﻿with open('lib/screens/chat_screen.dart', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace("          import 'dart:convert';", "")
-
-with open('lib/screens/chat_screen.dart', 'w', encoding='utf-8') as f:
-    f.write(content)
-print("Removed block import dart:convert")
